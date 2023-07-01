166811464262405763bb38055edfc6ba5bcfbf4b
Green Camper <stefan.palacios@gmail.com>
2023-07-01 16:22:46 -0500

    Re-added React starting public folder

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div className="flex-row justify-center">
+        <div
+          className="col-12 col-md-10 mb-3 p-3"
+          style={{ border: "1px dotted #1a1a1a" }}
+        >
+          
+        </div>
+        <div className="col-12 col-md-8 mb-3">
+          
+        </div>
+      </div>
+    </main>
+  );
+};
