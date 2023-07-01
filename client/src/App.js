e4e80bbbbffbc1d5175e765d6b1bc13f4f183e47
Green Camper <stefan.palacios@gmail.com>
2023-07-01 16:11:00 -0500

    Added set up code for client/app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+// Apollo client methods
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Components
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Request middleware
+const authLink = setContext((_, { headers }) => {
+  // Get auth token from local storage
+  const token = localStorage.getItem("id_token");
+  // Return headers for httpLink request
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Apollo client
+const client = new ApolloClient({
+  //authLink middleware to attach the JWT token to every request
+  link: authLink.concat(httpLink),
+  // Set up the cache
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
