142ac1efb568a305b23de6214bfbc7d4c8f6f867
Change Agent <keasiley@icloud.com>
2023-07-01 15:29:13 -0500

    typeDefs modified

--- server/schema/typedefs.js
+++ server/schema/typedefs.js
@@ -1,13 +1,47 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type User {
-    # These are all fields we can access from our user model
+  
+type User {
     _id: ID
     username: String
     email: String
     password: String
   }
-`;
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
+  type CalendarEvent {
+    _id: ID!
+    title: String!
+    description: String!
+    startDate: String!
+    endDate: String!
+    location: String!
+    userID: ID!
+  }
+  
+  type Query {
+    getCalendarEvent(eventID: ID!): CalendarEvent!
+    getAllCalendarEvents: [CalendarEvent]!
+    getUser(username: String!): User!
+    getUsers: [User]!
+  }
+   
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth!
+    login(email: String!, password: String!): Auth!
+    addCalendarEvent(title: String!, description: String!, startDate: String!, endDate: String!, location: String!, userID: ID!): CalendarEvent!
+    updateCalendarEvent(eventID: ID!, title: String, description: String, startDate: String, endDate: String, location: String, userID: ID!): CalendarEvent!
+    deleteCalendarEvent(eventID: ID!): CalendarEvent!
+    deleteAllCalendarEvents: [CalendarEvent]!
+    deleteUser(username: String!): User!
+    deleteAllUsers: [User]!
+  }
+    
+  `;
 
 module.exports = typeDefs;
