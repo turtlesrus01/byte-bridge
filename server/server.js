6187edc3fc97c773958a190913d7814a7556782d
Change Agent <keasiley@icloud.com>
2023-07-02 16:50:10 -0500

    server.js modfied to include code for nodemailer

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
+const nodemailer = require('nodemailer');
 const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
+require('dotenv').config();
 
 //Port for the server
 const PORT = process.env.PORT || 3001;
@@ -37,6 +38,40 @@ const startApolloServer = async () => {
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
   })
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL,
+      pass: process.env.WORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+   });
+   transporter.verify((err, success) => {
+    err
+      ? console.log(err)
+      : console.log(`=== Server is ready to take messages: ${success} ===`);
+   });
+
+   let mailOptions = {
+    from: "test@gmail.com",
+    to: process.env.EMAIL,
+    subject: "Nodemailer API",
+    text: "Hi from your nodemailer API",
+   };
+   transporter.sendMail(mailOptions, function (err, data) {
+    console.log("Recipient Email:", process.env.EMAIL);
+
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+      
+    }
+  });
   };
 //Start Apollo server
 startApolloServer();
