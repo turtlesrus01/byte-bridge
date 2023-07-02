673bfee55495c1501e27c84577f3ce69403e7bbd
Green Camper <stefan.palacios@gmail.com>
2023-07-01 23:04:22 -0500

    Added auth.js for front-end, changed packages to build successfully

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-const App = () => {
+const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: "1px dotted #1a1a1a" }}
-        >
-          
-        </div>
-        <div className="col-12 col-md-8 mb-3">
-          
-        </div>
+        ></div>
+        <div className="col-12 col-md-8 mb-3"></div>
       </div>
     </main>
   );
 };
+
+export default Home;
---
+++ client/src/utils/auth.js
@@ -0,0 +1,54 @@
+import decode from "jwt-decode";
+
+class AuthService {
+  getProfile() {
+    try {
+      return decode(this.getToken());
+    } catch (err) {
+      console.error("error decoding token", err);
+      //Returns null in case of an error
+      return null;
+    }
+  }
+
+  loggedIn() {
+    try {
+      const token = this.getToken();
+      return token && !this.isTokenExpired(token) ? true : false;
+    } catch (err) {
+      console.error("error checking login status", err);
+      return false;
+    }
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem("id_token");
+        return true;
+      }
+      return false;
+    } catch (err) {
+      console.error("error decoding token", err);
+      //return true to force token expiration
+      return true;
+    }
+  }
+
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
+
+  login(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
