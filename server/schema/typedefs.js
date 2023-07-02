4715acc1e3c9ae47a1f6851bee146a31e50b65c4
Green Camper <stefan.palacios@gmail.com>
2023-07-02 15:21:33 -0500

    Modified typedefs to consolidate queries

--- server/schema/typedefs.js
+++ server/schema/typedefs.js
@@ -1,53 +1,54 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  
 type User {
-    _id: ID
-    username: String
-    email: String
-    password: String
-  }
+  _id: ID
+  username: String
+  email: String
+  password: String
+}
 
-  type Auth {
-    token: ID!
-    User: User
-  }
-  
-  type CalendarEvent {
-    _id: ID!
+type Auth {
+  token: ID!
+  User: User
+}
+
+type CalendarEvent {
+  _id: ID!
+  title: String!
+  description: String!
+  startDate: String!
+  endDate: String!
+  location: String!
+  userID: ID!
+}
+
+type Query {
+  users: [User]
+  user(username: String!): User
+  calendarEvent(_id: ID!): User
+  calendarEvents: [User]
+  getCalendarEvent(eventID: ID!): CalendarEvent!
+  getUser(username: String!): User!
+  getUsers: [User]!
+}
+
+type Mutation {
+  addUser(username: String!, email: String!, password: String!): Auth!
+  login(email: String!, password: String!): Auth!
+  addCalendarEvent(
     title: String!
     description: String!
     startDate: String!
     endDate: String!
     location: String!
     userID: ID!
-  }
-  
-  type Query {
-    getCalendarEvent(eventID: ID!): CalendarEvent!
-    getUser(username: String!): User!
-    getUsers: [User]!
-  }
-
-  type Query {
-    users: [User]
-    user(username: String!): User
-    calendarEvent(_id: ID!): User
-    calendarEvents: [User]
-  }
-    
-   
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth!
-    login(email: String!, password: String!): Auth!
-    addCalendarEvent(title: String!, description: String!, startDate: String!, endDate: String!, location: String!, userID: ID!): CalendarEvent!
-    deleteCalendarEvent(eventID: ID!): CalendarEvent!
-    deleteAllCalendarEvents: [CalendarEvent]!
-    deleteUser(username: String!): User!
-    deleteAllUsers: [User]!
-  }
-    
-  `;
+  ): CalendarEvent!
+  deleteCalendarEvent(eventID: ID!): CalendarEvent!
+  deleteAllCalendarEvents: [CalendarEvent]!
+  deleteUser(username: String!): User!
+  deleteAllUsers: [User]!
+}
+`;
 
 module.exports = typeDefs;
