6625e36417507972faa2456ad388b29c81ab97c8
Change Agent <keasiley@icloud.com>
2023-07-09 13:35:41 -0500

    one calendar now displaying

--- client/src/components/TestCalendar/TestCalendar.js
+++ client/src/components/TestCalendar/TestCalendar.js
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "../../../src/App.css";
-//MUI
 import { Typography } from "@mui/material";
-// import 'react-calendar/dist/Calendar.css';
 
 function TestCalendar() {
-  const [date, setDate] = useState(
-    new Date([
-      new Date().getFullYear(),
-      new Date().getMonth(),
-      new Date().getDate(),
-    ])
-  );
+  const [date, setDate] = useState(new Date());
+
+  const handleDateChange = (selectedDate) => {
+    setDate(selectedDate);
+  };
+
+  const renderSelectedDate = () => {
+    if (Array.isArray(date)) {
+      return (
+        <Typography variant="body1" align="center">
+          <span className="bold">Start:</span> {date[0].toDateString()} |{" "}
+          <span className="bold">End:</span> {date[1].toDateString()}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography variant="body1" align="center">
+          <span className="bold">Selected date:</span> {date.toDateString()}
+        </Typography>
+      );
+    }
+  };
 
   return (
     <div className="app">
@@ -20,32 +33,9 @@ function TestCalendar() {
         Book Your Appointment with Your Realtor
       </Typography>
       <div className="calendar-container">
-        <Calendar onChange={setDate} value={date} selectRange={true} />
+        <Calendar onChange={handleDateChange} value={date} selectRange={true} />
       </div>
-      {date.length > 0 ? (
-        <Typography variant="body1" align="center">
-          <span className="bold">Start:</span> {date[0].toDateString()}
-          &nbsp;|&nbsp;
-          <span className="bold">End:</span> {date[1].toDateString()}
-        </Typography>
-      ) : (
-        <Typography variant="body1" align="center">
-          <span className="bold">Default selected date:</span>{" "}
-          {date.toDateString()}
-        </Typography>
-      )}
-      <Calendar
-        onChange={setDate}
-        value={date}
-        nextLabel="month>>"
-        nextAriaLabel="Go to next month"
-        next2Label="year>>"
-        next2AriaLabel="Go to next year"
-        prevLabel="<<month"
-        prevAriaLabel="Go to prev month"
-        prev2Label="<<year"
-        prev2AriaLabel="Go to prev year"
-      />
+      {renderSelectedDate()}
     </div>
   );
 }
