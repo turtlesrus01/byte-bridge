7de40fc622c4d041792a48d84850a4d75f747787
Green Camper <stefan.palacios@gmail.com>
2023-07-09 13:56:13 -0500

    Added reducer/context for homepage rendering

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+// Import the UserContext
+import { UserContext } from "../utils/UserContext"; 
+// Import the components
+import { CalendarEmail } from "./CalendarEmail";
 
 const Home = () => {
+  // Access the state from UserContext
+  const { state } = useContext(UserContext); 
+
   return (
     <main>
       <div className="flex-row justify-center">
@@ -10,7 +17,14 @@ const Home = () => {
         >
           <h2>HOMEPAGE</h2>
         </div>
-        <div className="col-12 col-md-8 mb-3"></div>
+        <div className="col-12 col-md-8 mb-3">
+          {state.isLoggedIn ? ( 
+            <CalendarEmail /> // Render the CalendarEmail component if logged in
+          ) : (
+            // Render the default content if not logged in
+            <div>Splashpage content</div>
+          )}
+        </div>
       </div>
     </main>
   );
---
+++ client/src/utils/UserContext.js
@@ -0,0 +1,39 @@
+//react context import
+import React, { createContext, useReducer } from "react";
+
+// Defines the intial state
+const initialState = {
+  isLoggedIn: false
+};
+
+// Login action type
+const SET_LOGIN_STATUS = 'SET_LOGIN_STATUS';
+
+//useReducer to initialize state
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOGIN_STATUS:
+      return {
+        ...state,
+        isLoggedIn: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+//UserContext initial state
+export const UserContext = createContext();
+
+//Create the provider component
+export const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+
