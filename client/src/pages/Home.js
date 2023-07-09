a2b8e081bbccbac20c27407c71f5439e9cfcc9db
Green Camper <stefan.palacios@gmail.com>
2023-07-09 16:30:39 -0500

    Added more MUI styling for Homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,39 +1,51 @@
 import React, { useContext } from "react";
 // Import the UserContext
-import { UserContext, SET_LOGIN_STATUS } from "../utils/UserContext"; 
+import { UserContext, SET_LOGIN_STATUS } from "../utils/UserContext";
 // Import the components
 import CalendarEmail from "./CalendarEmail";
 import { Button, Typography, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
 
 const Home = () => {
   // Access the state from UserContext
-  const { state, dispatch } = useContext(UserContext); 
+  const { state, dispatch } = useContext(UserContext);
 
   const handleLogin = () => {
     // Dispatch action to set login status to true
-    dispatch({ type: SET_LOGIN_STATUS, payload: true }); 
+    dispatch({ type: SET_LOGIN_STATUS, payload: true });
+  };
+
+  const handleLogout = () => {
+    Auth.logout();
+    dispatch({ type: SET_LOGIN_STATUS, payload: false });
   };
 
   return (
     <Box display="flex" justifyContent="center">
-      <Box
-        className="col-12 col-md-10 mb-3 p-3"
-        sx={{ border: "1px dotted #1a1a1a" }}
-      >
-        <Typography variant="h2" component="h2">
-          HOMEPAGE
-        </Typography>
-        <Button variant="contained" onClick={handleLogin}>
-          Login
-        </Button>
-      </Box>
-      <Box className="col-12 col-md-8 mb-3">
-        {state.isLoggedIn ? (
+      {state.isLoggedIn ? (
+        <Box className="col-12 col-md-8 mb-3">
           <CalendarEmail />
-        ) : (
-          <Typography variant="body1">Default Content</Typography>
-        )}
-      </Box>
+          <Button variant="outlined" onClick={handleLogout}>
+            Logout
+          </Button>
+        </Box>
+      ) : (
+        <>
+          <Box className="col-12 col-md-10 mb-3 p-3" sx={{ border: "1px dotted #1a1a1a" }}>
+            <Typography variant="h3" component="h3">
+              HOMEPAGE
+            </Typography>
+            <Button variant="contained" onClick={handleLogin}>
+              Login
+            </Button>
+          </Box>
+          <Box className="col-12 col-md-8 mb-3">
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
