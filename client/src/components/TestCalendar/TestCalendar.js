1a336f5ddf2a56e7bbfdfea987fc2b1fa51ccf9d
Change Agent <keasiley@icloud.com>
2023-07-09 15:08:24 -0500

    calendar has CRUD operations

--- client/src/components/TestCalendar/TestCalendar.js
+++ client/src/components/TestCalendar/TestCalendar.js
@@ -1,14 +1,62 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "../../../src/App.css";
 import { Typography } from "@mui/material";
+import { addCalendarEvent, deleteCalendarEvent, deleteAllCalendarEvents, UpdateCalendarEvent } from "../../../src/utils/mutations";
 
 function TestCalendar() {
   const [date, setDate] = useState(new Date());
 
-  const handleDateChange = (selectedDate) => {
+  const handleDateChange = async (selectedDate) => {
     setDate(selectedDate);
-  };
+
+    try {
+      const response = await addCalendarEvent(selectedDate); // Call the addCalendarEvent function with the selected date
+      console.log("Event added:", response);
+      // Perform any other operations or display notifications/alerts based on the response
+    } catch (error) {
+      console.error("Error adding event:", error);
+      // Display an error notification or handle the error gracefully
+    }
+  
+  try {
+    const response = await addCalendarEvent(date); // Call the addCalendarEvent function with the selected date
+    console.log("Event added:", response);
+    // Perform any other operations or display notifications/alerts based on the response
+  } catch (error) {
+    console.error("Error adding event:", error);
+    // Display an error notification or handle the error gracefully
+  }
+  try {
+    const response = await deleteCalendarEvent(date); // Call the addCalendarEvent function with the selected date
+    console.log("Event added:", response);
+    // Perform any other operations or display notifications/alerts based on the response
+  } catch (error) {
+    console.error("Error adding event:", error);
+    // Display an error notification or handle the error gracefully
+  }
+  try {
+    const response = await deleteAllCalendarEvents(); // Call the addCalendarEvent function with the selected date
+    console.log("Event added:", response);
+    // Perform any other operations or display notifications/alerts based on the response
+  } catch (error) {
+    console.error("Error adding event:", error);
+    // Display an error notification or handle the error gracefully
+  }
+  try {
+    const response = await UpdateCalendarEvent(date); // Call the addCalendarEvent function with the selected date
+    console.log("Event added:", response);
+    // Perform any other operations or display notifications/alerts based on the response
+  } catch (error) {
+    console.error("Error adding event:", error);
+    // Display an error notification or handle the error gracefully
+  }
+};
+
+
+  
+
+
 
   const renderSelectedDate = () => {
     if (Array.isArray(date)) {
