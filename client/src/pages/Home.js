fe73c004688db335b9bf8ae30ef50b5960409bed
Green Camper <stefan.palacios@gmail.com>
2023-07-11 20:18:44 -0500

    Added more MUI styling to splash page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,9 +3,10 @@ import React, { useContext } from "react";
 import { UserContext, SET_LOGIN_STATUS } from "../utils/UserContext";
 // Import the components
 import CalendarEmail from "./CalendarEmail";
-import { Button, Typography, Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Button, Typography, Box, Grid, Container} from "@mui/material";
+//import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
+import hero from "../assets/img/pexels-fabian-reitmeier-707915.jpg";
 
 const Home = () => {
   // Access the state from UserContext
@@ -32,21 +33,38 @@ const Home = () => {
         </Box>
       ) : (
         <>
-          <Box
-            className="col-12 col-md-10 mb-3 p-3"
-            sx={{ border: "1px dotted #1a1a1a" }}
-          >
-            <Typography variant="h3" component="h3">
-              HOMEPAGE
-            </Typography>
-            <Button variant="contained" onClick={handleLogin}>
-              Login
-            </Button>
-          </Box>
-          <Box className="col-12 col-md-8 mb-3">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </Box>
+          <Container maxWidth="lg">
+      <Box className="col-12 col-md-6 mb-3 p-3">
+        <Box mb={3}>
+          <Typography variant="h3" component="h3">
+            HOMEPAGE
+          </Typography>
+        </Box>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item xs={12} sm={6}>
+            <Box pr={2}>
+              <img
+                src={hero}
+                alt="Nature Hero"
+                style={{ width: "100%", maxWidth: "100%" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Box>
+              <Typography variant="body1" paragraph='true'>
+              Byte-Bridge is a cutting-edge scheduling app designed specifically for realtors and home buyers. With Byte-Bridge, the process of scheduling property viewings and appointments becomes seamless and efficient. Realtors can easily manage their listings, availability, and client appointments all in one place, while home buyers can conveniently browse available properties and request viewings. 
+              <br/>
+              The app provides a user-friendly interface, allowing realtors and home buyers to communicate and coordinate effortlessly. Byte-Bridge streamlines the scheduling process, saving time and reducing the stress often associated with property viewings. Experience a new level of convenience and organization with Byte-Bridge for all your real estate scheduling needs.
+              </Typography>
+              <Button variant="contained" onClick={handleLogin}>
+                Login
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
         </>
       )}
     </Box>
